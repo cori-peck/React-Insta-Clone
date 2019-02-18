3e981b9d794fa40138332d46db06ca0fdfbea379
cori-peck <cori.peck@ymail.com>
2019-02-18 17:42:00 -0600

    styled searchbar

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import * as Icon from 'react-feather';
+import './SearchBar.css';
 
 function SearchBar() {
     return (
-        <div>
-            <div>
-                <Icon.Instagram />
-                <img src="https://fontmeme.com/images/instagram-new-logo.png" alt="Instagram Cursive Logo" />
+        <div className="sbContainer">
+            <div className="sbLT">
+                <Icon.Instagram className="instaIcon" />
+                <img className="logo" src="https://fontmeme.com/images/instagram-new-logo.png" alt="Instagram Cursive Logo" />
             </div>
             <div>
                 <form>
-                    <input type="text" placeholder="🔍 Search" />
+                    <input className="sbSearch" type="text" placeholder="🔍 Search" />
                 </form>
             </div>
-            <div>
-                <Icon.Compass />
-                <Icon.Heart />
-                <Icon.User />
+            <div className="sbRT">
+                <Icon.Compass className="navIcons" />
+                <Icon.Heart className="navIcons" />
+                <Icon.User className="navIcons" />
             </div>
         </div>
     )
